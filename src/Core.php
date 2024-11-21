ee86633d4d231b54b3d6f2e64942f970d26026ec
jamesgober <james.gober@icloud.com>
2024-11-21 02:31:20 -0500

    Initial Update

--- src/Core.php
+++ src/Core.php
@@ -1,9 +1,40 @@
-<?php
+<?php /*
+¸_______¸__¸ ¸__¸___¸   ¸_______¸_______¸
+┊   _   ┊   Y   ┊   ┊   ┊       ┊       ┊
+┊   l   ┊   ┊   ┊   ┊   ┊   |___┊  [____┊
+┊   ____┊   ┊   ┊   ┊___┊___¸   ┊  ¸__]_ 
+┊   ┊   ┊   |   ┊   l   ┊   ]   ┊  [    ┊    
+┊   ┊   ┊       ┊       ┊       ┊       ┊
+'---`   `-------^-------^-------^-------'
+░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+ARTEX PULSE ⦙⦙⦙⦙ APPLICATION FRAMEWORK CORE
+░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░ */
+/**
+ * Artex Pulse - Easiest Static Blog Generator
+ * 
+ * @author      James Gober <james@jamesgober.com>
+ * @copyright   2025 Artex Agency Inc.
+ * @link        https://artexagency.com/
+ * @version     1.0.0
+ */
+declare(strict_types=1);
 
-namespace Artex\Essence;
+namespace Essence\Core;
 
-use \Artex\Essence\Engine\Bootstrap;
+use \Essence\Essence;
+use \Essence\Core\Bootstrap;
 
+/**
+ * Core
+ * 
+ * @package    Essence\Core
+ * @category   Core
+ * @access     public
+ * @version    1.0.0
+ * @since      1.0.0
+ * @link       https://artexessence.com/pulse/ Project Website
+ * @license    Artex Permissive Software License (APSL)
+ */
 class Core
 {
 
